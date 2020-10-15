3085b693e8b80a029be3e2262d200656770445d4
user <user@DESKTOP-09GNB44>
2020-10-15 17:56:51 +0900

    homework2_1

--- CaJava2010/src/operatorEx/Homework2_1.java
+++ CaJava2010/src/operatorEx/Homework2_1.java
@@ -4,8 +4,8 @@ public class Homework2_1 {
 
 	public static void main(String[] args) {
 		int pencil = 1000;
-		System.out.println("학생당 "+1000/33+"개");
-		System.out.println("나머지 "+1000%33+"개");
+		System.out.println("학생당 "+pencil/33+"개");
+		System.out.println("나머지 "+pencil%33+"개");
 		
 	}
 
