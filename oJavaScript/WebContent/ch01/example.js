9668d925cebf87eb84ba0f84b99642d123edae18
20201130 <user@DESKTOP-6QFIM93>
2020-11-30 17:49:09 +0900

    JavaScript

---
+++ oJavaScript/WebContent/ch01/example.js
@@ -0,0 +1,8 @@
+/**
+ example.js 
+ */
+document.write("example.js 내부...");
+/*function printDate(){
+	document.write("example.js 내부...");
+	//document.getElementById("date").innerHTML = Date();
+}*/
